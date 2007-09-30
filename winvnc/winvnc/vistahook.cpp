5611f12585c89b63218d4f57266ba9f9012d2149
cyberfox123 <cyberfox123@0bd26501-c230-0410-ab9e-a7946180fe3b>
2007-09-30 13:42:39 +0000

    place cad in seperate thread
    git-svn-id: https://svn.code.sf.net/p/ultravnc/code/UltraVNC%20Project%20Root/UltraVNC@106 0bd26501-c230-0410-ab9e-a7946180fe3b

--- winvnc/winvnc/vistahook.cpp
+++ winvnc/winvnc/vistahook.cpp
@@ -162,3 +162,50 @@ vncDesktopThread::Handle_Ringbuffer(mystruct *ringbuffer,rfb::Region2D &rgncache
 	g_Oldcounter=counter;
 	return 1;
 }
+
+DWORD WINAPI Cadthread(LPVOID lpParam)
+{
+	HDESK desktop;
+	//vnclog.Print(LL_INTERR, VNCLOG("SelectDesktop \n"));
+	//vnclog.Print(LL_INTERR, VNCLOG("OpenInputdesktop2 NULL\n"));
+	desktop = OpenInputDesktop(0, FALSE,
+								DESKTOP_CREATEMENU | DESKTOP_CREATEWINDOW |
+								DESKTOP_ENUMERATE | DESKTOP_HOOKCONTROL |
+								DESKTOP_WRITEOBJECTS | DESKTOP_READOBJECTS |
+								DESKTOP_SWITCHDESKTOP | GENERIC_WRITE
+								);
+
+	if (desktop == NULL)
+		vnclog.Print(LL_INTERR, VNCLOG("OpenInputdesktop Error \n"));
+	else 
+		vnclog.Print(LL_INTERR, VNCLOG("OpenInputdesktop OK\n"));
+
+	HDESK old_desktop = GetThreadDesktop(GetCurrentThreadId());
+	DWORD dummy;
+
+	char new_name[256];
+
+	if (!GetUserObjectInformation(desktop, UOI_NAME, &new_name, 256, &dummy))
+	{
+		vnclog.Print(LL_INTERR, VNCLOG("!GetUserObjectInformation \n"));
+	}
+
+	vnclog.Print(LL_INTERR, VNCLOG("SelectHDESK to %s (%x) from %x\n"), new_name, desktop, old_desktop);
+
+	if (!SetThreadDesktop(desktop))
+	{
+		vnclog.Print(LL_INTERR, VNCLOG("SelectHDESK:!SetThreadDesktop \n"));
+	}
+	char user[150];
+	DWORD length=150;
+	GetUserName(user, &length);
+
+	if (!CloseDesktop(old_desktop))
+		vnclog.Print(LL_INTERR, VNCLOG("SelectHDESK failed to close old desktop %x (Err=%d) %s\n"), old_desktop, GetLastError(),user);
+
+
+	ShellExecute(GetDesktopWindow(), "open", "cad.exe", "", 0, SW_SHOWNORMAL);
+
+	CloseDesktop(desktop);
+	return 0;
+}
