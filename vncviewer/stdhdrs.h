89cbd10cd389c54ab40a4936120aa95029049f60
cyberfox123 <cyberfox123@0bd26501-c230-0410-ab9e-a7946180fe3b>
2008-02-03 21:12:26 +0000

    VS 2005/2008  X64
    git-svn-id: https://svn.code.sf.net/p/ultravnc/code/UltraVNC%20Project%20Root/UltraVNC@202 0bd26501-c230-0410-ab9e-a7946180fe3b

--- vncviewer/stdhdrs.h
+++ vncviewer/stdhdrs.h
@@ -38,7 +38,7 @@
 #include "rfb.h"
 
 #ifndef _X64
-#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
+//#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #else
 #pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='amd64' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #endif
