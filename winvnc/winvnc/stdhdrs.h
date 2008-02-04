a9f58098bf7b97900680e8c7941c8ba0e886c4c8
cyberfox123 <cyberfox123@0bd26501-c230-0410-ab9e-a7946180fe3b>
2008-02-04 20:32:17 +0000

    X64 manifest error fix
    git-svn-id: https://svn.code.sf.net/p/ultravnc/code/UltraVNC%20Project%20Root/UltraVNC@231 0bd26501-c230-0410-ab9e-a7946180fe3b

--- winvnc/winvnc/stdhdrs.h
+++ winvnc/winvnc/stdhdrs.h
@@ -68,6 +68,10 @@ extern VNCLog vnclog;
 // Macros for sticking in the current file name
 #define VNCLOG(s)	(__FILE__ " : " s)
 
-
+#ifndef _X64
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#else
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='amd64' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#endif
 //#define memcpy memcpy_amd
 //remove comment to compiler for >=athhlon  or >=PIII
