2007530eae6dedff63bea5b22e15b3ff6fb4cf8c
cyberfox123 <cyberfox123@0bd26501-c230-0410-ab9e-a7946180fe3b>
2008-03-21 20:21:45 +0000

    git-svn-id: https://svn.code.sf.net/p/ultravnc/code/UltraVNC%20Project%20Root/UltraVNC@271 0bd26501-c230-0410-ab9e-a7946180fe3b

--- vncviewer/ClientConnection.cpp
+++ vncviewer/ClientConnection.cpp
@@ -2265,7 +2265,10 @@ void ClientConnection::SizeWindow()
 		nLocalHeight -= TitleBarHeight;
 		//if (m_opts.m_ShowToolbar)
 		nLocalHeight -= (m_TBr.bottom); // Always take toolbar into account in calculation
-		m_opts.m_scale_num = (int)((nLocalHeight * 100) / m_si.framebufferHeight);
+		int verticalRatio = (int)((nLocalHeight * 100) / m_si.framebufferHeight);
+		int horizontalRatio = (int)((nLocalWidth * 100) / m_si.framebufferWidth);
+		m_opts.m_scale_num = min(verticalRatio, horizontalRatio);
+
 		m_opts.m_scale_den = 100;
 		m_opts.m_scaling = true; 
 		m_fScalingDone = true;
