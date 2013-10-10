7b91bc6a267e1cd63b3e7c5f37391d7fc4705da0
cyberfox123 <cyberfox123@0bd26501-c230-0410-ab9e-a7946180fe3b>
2013-10-10 21:11:52 +0000

    turbo jpeg update
    git-svn-id: https://svn.code.sf.net/p/ultravnc/code/UltraVNC%20Project%20Root/UltraVNC@829 0bd26501-c230-0410-ab9e-a7946180fe3b

--- libjpeg-turbo-win/jdsample.c
+++ libjpeg-turbo-win/jdsample.c
@@ -36,34 +36,26 @@ typedef JMETHOD(void, upsample1_ptr,
 /* Private subobject */
 
 typedef struct {
-  struct jpeg_upsampler pub;	/* public fields */
+  struct jpeg_upsampler pub;	
 
-  /* Color conversion buffer.  When using separate upsampling and color
-   * conversion steps, this buffer holds one upsampled row group until it
-   * has been color converted and output.
-   * Note: we do not allocate any storage for component(s) which are full-size,
-   * ie do not need rescaling.  The corresponding entry of color_buf[] is
-   * simply set to point to the input data array, thereby avoiding copying.
-   */
+ 
   JSAMPARRAY color_buf[MAX_COMPONENTS];
 
-  /* Per-component upsampling method pointers */
+ 
   upsample1_ptr methods[MAX_COMPONENTS];
 
-  int next_row_out;		/* counts rows emitted from color_buf */
-  JDIMENSION rows_to_go;	/* counts rows remaining in image */
+  int next_row_out;		
+  JDIMENSION rows_to_go;	
 
-  /* Height of an input row group for each component. */
+  
   int rowgroup_height[MAX_COMPONENTS];
 
-  /* These arrays save pixel expansion factors so that int_expand need not
-   * recompute them each time.  They are unused for other upsampling methods.
-   */
+ 
   UINT8 h_expand[MAX_COMPONENTS];
   UINT8 v_expand[MAX_COMPONENTS];
-} my_upsampler;
+} my_upsampler2;
 
-typedef my_upsampler * my_upsample_ptr;
+typedef my_upsampler2 * my_upsample_ptr;
 
 
 /*
