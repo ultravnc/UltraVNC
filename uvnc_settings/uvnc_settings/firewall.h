4d64dad4cd5c59fac5784598cff76be83df2c90f
cyberfox123 <cyberfox123@0bd26501-c230-0410-ab9e-a7946180fe3b>
2016-01-23 12:33:13 +0000

    git-svn-id: https://svn.code.sf.net/p/ultravnc/code/UltraVNC%20Project%20Root/UltraVNC@1000 0bd26501-c230-0410-ab9e-a7946180fe3b

--- uvnc_settings/uvnc_settings/firewall.h
+++
@@ -1,6 +0,0 @@
-void FirewallCheck(HWND hwnd);
-void CheckServiceState();
-bool ControlSSDPService(bool start);
-bool ControlUPnPPorts(bool open);
-bool IsICSConnEnabled();
-void checksetport(int port);
--- uvnc_settings/uvnc_settings/log.h
+++
@@ -1,1 +0,0 @@
-void debug( const char *fmt, ... );
