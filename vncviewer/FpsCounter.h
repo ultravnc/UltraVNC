f778d9a3ac3ff877f54eec20f5fd86d1ecc6ea42
cyberfox123 <cyberfox123@0bd26501-c230-0410-ab9e-a7946180fe3b>
2019-03-20 19:05:24 +0000

    missing file
    git-svn-id: https://svn.code.sf.net/p/ultravnc/code/UltraVNC%20Project%20Root/UltraVNC@1217 0bd26501-c230-0410-ab9e-a7946180fe3b

---
+++ vncviewer/FpsCounter.h
@@ -0,0 +1,61 @@
+#include <Windows.h>
+
+class Interval
+{
+private:
+    unsigned int initial_;
+
+public:
+    // Ctor
+    inline Interval() : initial_(GetTickCount())
+    {
+    }
+
+    // Dtor
+    virtual ~Interval()
+    {
+    }
+
+    inline unsigned int value() const
+    {
+        return GetTickCount()-initial_;
+    }
+};
+
+class Fps
+{
+protected:
+    unsigned int m_fps;
+    unsigned int m_fpscount;
+    Interval m_fpsinterval;
+
+public:
+    // Constructor
+    Fps() : m_fps(0), m_fpscount(0)
+    {
+    }
+
+    // Update
+    void update()
+    {
+        // increase the counter by one
+        m_fpscount++;
+
+        // one second elapsed? (= 1000 milliseconds)
+        if (m_fpsinterval.value() > 1000)
+        {
+            // save the current counter value to m_fps
+            m_fps         = m_fpscount;
+           
+            // reset the counter and the interval
+            m_fpscount    = 0;
+            m_fpsinterval = Interval();
+        }
+    }
+
+    // Get fps
+    unsigned int get() const
+    {
+        return m_fps;
+    }
+};
