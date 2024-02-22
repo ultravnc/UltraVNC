5ddf26a4136a204503ee42bf21104a0a868cd122
lbocquet <3823809+lbocquet@users.noreply.github.com>
2024-02-22 20:30:34 +0100

    setcad: Update of the code, harmonization, cleanup, ... (#171)
    setcad: Update of the code, harmonization, cleanup, ...

--- setcad/setcad/targetver.h
+++ setcad/setcad/targetver.h
@@ -1,8 +1,33 @@
+/////////////////////////////////////////////////////////////////////////////
+//  Copyright (C) 2002-2024 UltraVNC Team Members. All Rights Reserved.
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
+//  USA.
+//
+//  If the source code for the program is not available from the place from
+//  which you received this file, check
+//  https://uvnc.com/
+//
+////////////////////////////////////////////////////////////////////////////
+
+
 #pragma once
 
-// The following macros define the minimum required platform.  The minimum required platform
+// The following macros define the minimum required platform. The minimum required platform
 // is the earliest version of Windows, Internet Explorer etc. that has the necessary features to run 
-// your application.  The macros work by enabling all features available on platform versions up to and 
+// your application. The macros work by enabling all features available on platform versions up to and 
 // including the version specified.
 
 // Modify the following defines if you have to target a platform prior to the ones specified below.
