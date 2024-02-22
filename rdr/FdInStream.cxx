9b6e322d9f4772389088e386c259a324621836cc
lbocquet <3823809+lbocquet@users.noreply.github.com>
2024-02-22 19:36:39 +0100

    rdr: Update of the code, harmonization, cleanup, ... (#161)
    rdr: Update of the code, harmonization, cleanup, ...

--- rdr/FdInStream.cxx
+++ rdr/FdInStream.cxx
@@ -1,5 +1,6 @@
-// Copyright (C) 2002 UltraVNC Team Members. All Rights Reserved.
-// Copyright (C) 2002 RealVNC Ltd.  All Rights Reserved.
+/////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2002-2024 UltraVNC Team Members. All Rights Reserved.
+// Copyright (C) 2002 RealVNC Ltd. All Rights Reserved.
 //
 // This is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -15,6 +16,13 @@
 // along with this software; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
 // USA.
+//
+//  If the source code for the program is not available from the place from
+//  which you received this file, check
+//  https://uvnc.com/
+//
+////////////////////////////////////////////////////////////////////////////
+
 
 #include <stdio.h>
 #include <string.h>
