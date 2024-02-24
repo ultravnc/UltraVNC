260e2ddb583e6e16772a9f05f3884d54d3d468db
RudiDeVos <71467484+RudiDeVos@users.noreply.github.com>
2024-02-24 19:52:12 +0100

    update projectfile names Remove old project files

--- addon/logger/messages.h
+++ addon/logger/messages.h
@@ -21,10 +21,8 @@
 //  https://uvnc.com/
 //
 ////////////////////////////////////////////////////////////////////////////
-
-
 //
-//  Values are 32-bit values laid out as follows:
+//  Values are 32 bit values laid out as follows:
 //
 //   3 3 2 2 2 2 2 2 2 2 2 2 1 1 1 1 1 1 1 1 1 1
 //   1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0
