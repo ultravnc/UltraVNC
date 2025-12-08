f629413f2a7e04928cacb373819ae3c2b0000089
RudiDeVos <71467484+RudiDeVos@users.noreply.github.com>
2025-12-08 20:43:07 +0100

    revert layered windows old win10 possible fix

--- winvnc/winvnc/LayeredWindows.cpp
+++ winvnc/winvnc/LayeredWindows.cpp
@@ -216,8 +216,6 @@ bool LayeredWindows::create_black_window(void)
     SetWindowPos(hwnd, HWND_TOPMOST, x, y, cx, cy, SWP_FRAMECHANGED | SWP_NOACTIVATE);
     if (VNC_OSVersion::getInstance()->OS_WIN10_TRANS)
         SetWindowDisplayAffinity(hwnd, WDA_EXCLUDEFROMCAPTURE);
-    else
-        SetWindowDisplayAffinity(hwnd, WDA_MONITOR);
     return true;
 }
 
@@ -380,8 +378,6 @@ bool LayeredWindows::create_border_window(RECT rect)
     SetWindowPos(hwnd, HWND_TOPMOST, rect.left, rect.top, rect.right - rect.left, rect.bottom - rect.top, SWP_FRAMECHANGED | SWP_NOACTIVATE);
     if (VNC_OSVersion::getInstance()->OS_WIN10_TRANS)
         SetWindowDisplayAffinity(hwnd, WDA_EXCLUDEFROMCAPTURE);
-    else
-        SetWindowDisplayAffinity(hwnd, WDA_MONITOR);
     return true;
 }
 
@@ -391,7 +387,7 @@ bool LayeredWindows::SetBlankMonitor(bool enabled, bool blankMonitorEnabled, boo
     if (blankMonitorEnabled)
     {
 
-        /*if ((!VNC_OSVersion::getInstance()->OS_WIN10_TRANS && VNC_OSVersion::getInstance()->OS_WIN10)
+        if ((!VNC_OSVersion::getInstance()->OS_WIN10_TRANS && VNC_OSVersion::getInstance()->OS_WIN10)
             || VNC_OSVersion::getInstance()->OS_WIN8)
         {
             if (enabled) {
@@ -414,7 +410,7 @@ bool LayeredWindows::SetBlankMonitor(bool enabled, bool blankMonitorEnabled, boo
             }
 
         }
-        else*/ {
+        else {
             if (enabled) {
                 if (VNC_OSVersion::getInstance()->OS_AERO_ON)
                     VNC_OSVersion::getInstance()->DisableAero();
